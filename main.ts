2887300bf2198caa33e817ec7118018376657753
danny polonsky <dpolonsky@salesforce.com>
2024-12-14 14:44:00 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.forever(function () {
+    basic.showArrow(ArrowNames.North)
+    basic.showArrow(ArrowNames.NorthEast)
+    basic.showArrow(ArrowNames.East)
+    basic.showArrow(ArrowNames.South)
+})
